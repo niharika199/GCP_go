76987392064f9e49a8fbc1c03bc7322fa70da1fa
root <root@go1.us-central1-a.c.niharika.internal>
2020-02-19 12:53:55 +0000

    Logging

--- api/api.go
+++ api/api.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+//	"github.com/gorilla/mux"
 	network "github.com/niharika199/GCP_go/compute/network"
 	server "github.com/niharika199/GCP_go/compute/server"
 	"log"
@@ -11,20 +11,39 @@ import (
 	"os"
 )
 
+
+// GeneralLogger exported
+var GeneralLogger *log.Logger
+
+// ErrorLogger exported
+var AccessLogger *log.Logger
+
 func main() {
-	r := mux.NewRouter()
+//	r := mux.NewRouter()
 	//	r.HandleFunc("/", welcome)
-	file, err := os.OpenFile("../logs/gcp.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+        generalLog, err := os.OpenFile("../logs/gcp.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
-	}
-	log.SetOutput(file)
-	r.HandleFunc("/servercreate", create)
-	r.HandleFunc("/serverdelete", Delete)
-	r.HandleFunc("/networkcreate", netcreate)
-	r.HandleFunc("/networkdelete", netdel)
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+                }
+        accessLog, err := os.OpenFile("../logs/access.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+        if err != nil {
+                fmt.Println("Error opening file:", err)
+                os.Exit(1)
+                }
+        GeneralLogger = log.New(generalLog, "General Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+        AccessLogger = log.New(accessLog, "Request Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+//ile, err := os.OpenFile("../logs/gcp.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	log.SetOutput(file)
+	http.HandleFunc("/servercreate", create)
+	http.HandleFunc("/serverdelete", Delete)
+	http.HandleFunc("/networkcreate", netcreate)
+	http.HandleFunc("/networkdelete", netdel)
 	//      r.HandleFunc("/listservers",listservers)
-	if err := http.ListenAndServe(":80", r); err != nil {
+	if err := http.ListenAndServe(":80",logRequest(http.DefaultServeMux)) ; err != nil {
 		log.Fatal(err)
 	}
 }
@@ -34,6 +53,15 @@ func main() {
 	tmpl.Execute(w, nil)
 }*/
 
+
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		AccessLogger.Println(r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func create(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	input := server.Serverinput{}
@@ -42,8 +70,7 @@ func create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	serverout := input.Createserver()
-
-	log.Println("Created the instance", serverout.Name, serverout.PublicIP)
+	GeneralLogger.Println("Created the instance", serverout.Name, serverout.PublicIP)
 	fmt.Fprintln(w, "Created the server")
 	fmt.Fprintln(w, serverout.Name)
 	fmt.Fprintln(w, serverout.PublicIP)
@@ -59,7 +86,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	}
 	input.Deleteserver()
 	// add the code to handle the error if not found
-	log.Println("deleted the server", input.Name)
+	GeneralLogger.Println("deleted the server", input.Name)
 	fmt.Fprintln(w, "deleted the instance")
 }
 
